66e832a928d9d2c028e897f8a6bdd1508b52f963
ashvinjaiswal <ashvin.edu@gmail.com>
2021-04-10 21:58:55 +0530

    Create list with the Object

--- src/App.js
+++ src/App.js
@@ -9,21 +9,30 @@ class App extends Component{
 
     //set the state
     this.state = {
-      string: 'Ashvin Jaiswal'
-     
+      string: 'Ashvin Jaiswal',
+      monsters:[
+        {
+          id:"mon1", 
+          name:"Frankstig"
+        },
+        {
+          id:"mon2", 
+          name:"Dracula"
+        },
+        {
+          id:"mon3", 
+          name:"Zombie"
+        }
+      ]
     }
   }
 
  render(){
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to react world - {this.state.string}
-        </p>
-        <button onClick={() => this.setState({string: 'lets start'})}>Change the text</button>
-      </header>
+      {this.state.monsters.map((monster,index) =>
+          <h1 key={monster.id}> {monster.name}</h1>
+      )}      
     </div>
   );
  }
