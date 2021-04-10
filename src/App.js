7721baf3a4420e7ebb1cc60a0b62167cfbccc868
ashvinjaiswal <ashvin.edu@gmail.com>
2021-04-10 20:57:19 +0530

    Change function to Class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor(){
+    super();
+
+    //set the state
+    this.state = {
+      string: 'Ashvin Jaiswal'
+     
+    }
+  }
+
+ render(){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to react world - {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => this.setState({string: 'lets start'})}>Change the text</button>
       </header>
     </div>
   );
+ }
 }
-
 export default App;
