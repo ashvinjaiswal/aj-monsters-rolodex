a0d779f5c617035943b6c58c59b84262e28b9feb
ashvinjaiswal <ashvin.edu@gmail.com>
2021-04-10 23:51:30 +0530

    Creating Card list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component{
 
@@ -27,9 +28,11 @@ class App extends Component{
  render(){
     return (
     <div className="App">
-      {this.state.monsters.map((monster,index) =>
-          <h1 key={monster.id}> {monster.name}</h1>
-      )}      
+      <CardList name="test">
+        {this.state.monsters.map((monster,index) =>
+            <h1 key={monster.id}> {monster.name}</h1>
+        )}    
+      </CardList>       
     </div>
   );
  }
