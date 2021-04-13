ecab6175521c56847650cee725b033e31b45a26f
ashvinjaiswal <ashvin.edu@gmail.com>
2021-04-13 20:54:38 +0530

    Added Searchbox component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import './App.css';
 import { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-class App extends Component{
+class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
 
     //set the state
     this.state = {
       string: 'Ashvin Jaiswal',
-      monsters:[],
-      searchField:''
+      monsters: [],
+      searchField: ''
     }
   }
 
   //LifeCycle Called immediately after a component is mounted. 
   // Setting state here will trigger re-rendering.
-  componentDidMount(){
+  componentDidMount() {
     //fecth the data and render on the page
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(resp => resp.json())
-    .then(users => this.setState({monsters:users}))
-    
+      .then(resp => resp.json())
+      .then(users => this.setState({ monsters: users }))
+
   }
 
- render(){
+  render() {
 
-    const {monsters, searchField } = this.state;
+    const { monsters, searchField } = this.state;
     const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
-    <div className="App">
-      <input type='search' placeholder='seach monsters' onChange={e => this.setState({ searchField: e.target.value})}/>
-      <CardList monsters= {filterMonsters} />            
-    </div>
-  );
- }
+      <div className="App">
+        <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  }
 }
 export default App;
